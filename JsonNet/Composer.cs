5dbdb9ebbe95ef9b82bd401f8493a1ef3f688f4d
Pedro Rodrigues <prodrigues1990@gmail.com>
2016-10-24 18:53:08 +0100

    ComposeInt(), ComposeFloat(), ComposeNull(), ComposeBool() methods

--- JsonNet/Composer.cs
+++ JsonNet/Composer.cs
@@ -22,5 +22,44 @@ namespace JsonNet
 
             return sb.ToString();
         }
+
+        private static string ComposeInt(object value)
+        {
+            int var = (int)value;
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(var.ToString());
+
+            return sb.ToString();
+        }
+
+        private static string ComposeFloat(object value)
+        {
+            float var = (float)value;
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(var.ToString());
+
+            return sb.ToString();
+        }
+
+        private static string ComposeNull()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("null");
+
+            return sb.ToString();
+        }
+
+        private static string ComposeBool(object value)
+        {
+            bool var = (bool)value;
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(var.ToString());
+
+            return sb.ToString();
+        }
     }
 }
